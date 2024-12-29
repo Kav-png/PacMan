c9d839ee986967f7d5eb2ef36b001c45d75da9c8
Kavya <kavyakapadia@outlook.com>
2024-12-29 01:28:05 +0000

    Loaded and Rendered Images onto an Array linked to a HashSet

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("Pac man");
+        // frame.setVisible(false);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
